ec18dfcea7d4092c075de8fe4652fa541bb259c5
Mattia <matpolli93@gmail.com>
2021-04-09 16:22:57 +0200

    Draft milestone #1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 "use strict";
 
+// API Key
+const apiKey = "837fe6873adf73088b3b213a4902260e";
+
 const app = new Vue({
     el: "#app",
     data: {
-
-    },
-    mounted() {
-
+        userInput: "",
+        movies: []
     },
     methods: {
+        search() {
+            axios.get("https://api.themoviedb.org/3/search/movie?", {
+                params: {
+                    api_key: apiKey,
+                    query: this.userInput
+                }
+            }).then((response) => {
+                this.movies = response.data.results;
+                console.log(this.movies);
+            });
+
+            this.userInput = "";
+        }
+    },
+    mounted(){
 
     }
 });
