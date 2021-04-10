4f67af47b1dc9beb34f67f869d23f6f4e3ce5701
Mattia <matpolli93@gmail.com>
2021-04-10 10:07:10 +0200

    Add tv series search option and display language-flag (milestone #2)

--- js/script.js
+++ js/script.js
@@ -2,26 +2,45 @@
 
 // API Key
 const apiKey = "837fe6873adf73088b3b213a4902260e";
+// Flags
+const flagSeries = ["de", "en", "es", "fr", "it", "ja", "nl", "pt", "ru"];
 
 const app = new Vue({
     el: "#app",
     data: {
         userInput: "",
-        movies: []
+        tvTitles: [],
+        flags: [...flagSeries]
     },
     methods: {
+        // Method to search Movies and TV series
         search() {
+            // Movies
             axios.get("https://api.themoviedb.org/3/search/movie?", {
                 params: {
                     api_key: apiKey,
                     query: this.userInput
                 }
             }).then((response) => {
-                this.movies = response.data.results;
-                console.log(this.movies);
+                this.tvTitles = response.data.results;
             });
 
+            // TV series
+            axios.get("https://api.themoviedb.org/3/search/tv?", {
+                params: {
+                    api_key: apiKey,
+                    query: this.userInput
+                }
+            }).then((response) => {
+                this.tvTitles = [...this.tvTitles, ...response.data.results];
+            });
+
+            // Clearing user input after searching
             this.userInput = "";
+        },
+        // Method to display flags (languages)
+        displayFlags(str) {
+            return `img/flags/${str}.svg`;
         }
     },
     mounted(){
