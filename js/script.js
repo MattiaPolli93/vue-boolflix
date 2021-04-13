0c2ced8ee2dc0290505844faaad3e2563627fdc2
Mattia <matpolli93@gmail.com>
2021-04-13 14:59:21 +0200

    Edit style, add favicon, add navbar menu sections and improve JS code

--- js/script.js
+++ js/script.js
@@ -1,35 +1,24 @@
 "use strict";
 
-// Base URL
-const baseUrl = "https://api.themoviedb.org/3/"
-// API Key
-const apiKey = "837fe6873adf73088b3b213a4902260e";
-// Flags
-const flagSeries = ["de", "en", "es", "fr", "it", "ja", "ko", "nl", "pl", "pt", "ru", "zh"];
-
 const app = new Vue({
     el: "#app",
     data: {
+        baseUrl: "https://api.themoviedb.org/3/",
+        apiKey: "837fe6873adf73088b3b213a4902260e",
         userInput: "",
         tvTitles: [],
-        flags: [...flagSeries],
-        maxRating: 5
+        flags: ["de", "en", "es", "fr", "it", "ja", "ko", "nl", "pl", "pt", "ru", "zh"],
+        maxRating: 5,
+        navSections: ["Home", "Movies", "TV Series"],
+        sectionSelected: "Home"
     },
     methods: {
-        // Method to display daily trending Movies & TV series
-        trending() {
-            axios.get(baseUrl + "trending/all/day", {
-                params: {
-                    api_key: apiKey,
-                }
-            }).then((response) => this.tvTitles = response.data.results);
-        },
         // Method to search Movies and TV series
         search() {
             // Movies
-            axios.get(baseUrl + "search/movie", {
+            axios.get(this.baseUrl + "search/movie", {
                 params: {
-                    api_key: apiKey,
+                    api_key: this.apiKey,
                     query: this.userInput
                 }
             }).then((response) => {
@@ -37,9 +26,9 @@ const app = new Vue({
             });
 
             // TV series
-            axios.get(baseUrl + "search/tv", {
+            axios.get(this.baseUrl + "search/tv", {
                 params: {
-                    api_key: apiKey,
+                    api_key: this.apiKey,
                     query: this.userInput
                 }
             }).then((response) => {
@@ -49,6 +38,52 @@ const app = new Vue({
             // Clearing user input after searching
             this.userInput = "";
         },
+        // Method to display daily trending Movies & TV series
+        trending() {
+            axios.get(this.baseUrl + "trending/all/day", {
+                params: {
+                    api_key: this.apiKey,
+                }
+            }).then((response) => this.tvTitles = response.data.results);
+        },
+        // Method to search for most popular Movies
+        searchPopularMovies() {
+            axios.get(this.baseUrl + "movie/popular", {
+                params: {
+                    api_key: this.apiKey
+                }
+            }).then((response) => this.tvTitles = response.data.results);
+        },
+        // Method to search for most popular TV series
+        searchPopularTvSeries() {
+            axios.get(this.baseUrl + "tv/popular", {
+                params: {
+                    api_key: this.apiKey
+                }
+            }).then((response) => this.tvTitles = response.data.results);
+        },
+        // Method to access navbar menu sections
+        selectSection(section) {
+            this.sectionSelected = section;
+
+            // Selecting sections
+            switch(this.sectionSelected) {
+                // Home
+                case this.navSections[0]:
+                    this.trending();
+                break;
+
+                // Movies
+                case this.navSections[1]:
+                    this.searchPopularMovies();
+                break;
+
+                // TV series
+                case this.navSections[2]:
+                    this.searchPopularTvSeries();
+                break;
+            }
+        },
         // Method to display flags (languages)
         displayFlags(str) {
             return `img/flags/${str}.svg`;
@@ -56,10 +91,10 @@ const app = new Vue({
         // Method to round up the original x/10 to a x/5 rating
         roundRating(num) {
             return num = Math.ceil(num / 2);
-        }
+        }   
     },
     mounted(){
         // Displaying daily trending titles by default
         this.trending();
-    },
+    }
 });
