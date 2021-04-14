1e1d96e07af7d09fd94d6ee78cd3e8bfc3265144
Mattia <matpolli93@gmail.com>
2021-04-14 09:30:25 +0200

    Add switch-between-pages functionality, improve style and fix layout

--- js/script.js
+++ js/script.js
@@ -12,13 +12,16 @@ const app = new Vue({
         tvTitles: [],
         navSections: ["Home", "Trending", "Movies", "TV Series", "Discover"],
         sectionSelected: "Home",
+        otherSection: "",
         flags: ["de", "en", "es", "fr", "it", "ja", "ko", "nl", "pl", "pt", "ru", "zh"],
-        maxRating: 5
+        maxRating: 5,
+        pagesIndex: 1,
+        totalPages: null
     },
     methods: {
         // Method to search Movies and TV series
         search() {
-            // Making sure no nav menu section is selected before searching 
+            // Enabling no nav menu section to be selected before searching (to display input parameters)
             this.sectionSelected = "";
 
             // Movies
@@ -43,7 +46,7 @@ const app = new Vue({
 
             // Storing user input
             this.inputParams = this.userInput;
-            
+
             // Clearing user input after searching
             this.userInput = "";
         },
@@ -51,54 +54,81 @@ const app = new Vue({
         trendingDaily() {
             axios.get(this.baseUrl + "trending/all/day", {
                 params: {
-                    api_key: this.apiKey
+                    api_key: this.apiKey,
+                    page: this.pagesIndex
                 }
-            }).then((response) => this.tvTitles = response.data.results);
+            }).then((response) => {
+                this.tvTitles = response.data.results;
+                this.totalPages = response.data.total_pages;
+            });
         },
         // Method to display weekly trending Movies & TV series
         trendingWeekly() {
             axios.get(this.baseUrl + "trending/all/week", {
                 params: {
-                    api_key: this.apiKey                    
+                    api_key: this.apiKey,
+                    page: this.pagesIndex
                 }
-            }).then((response) => this.tvTitles = response.data.results);
+            }).then((response) => {
+                this.tvTitles = response.data.results;
+                this.totalPages = response.data.total_pages;
+            });
         },
         // Method to search for most popular Movies
         searchPopularMovies() {
             axios.get(this.baseUrl + "movie/popular", {
                 params: {
-                    api_key: this.apiKey
+                    api_key: this.apiKey,
+                    page: this.pagesIndex
                 }
-            }).then((response) => this.tvTitles = response.data.results);
+            }).then((response) => {
+                this.tvTitles = response.data.results;
+                this.totalPages = response.data.total_pages;
+            });
         },
         // Method to search for most popular TV series
         searchPopularTvSeries() {
             axios.get(this.baseUrl + "tv/popular", {
                 params: {
-                    api_key: this.apiKey
+                    api_key: this.apiKey,
+                    page: this.pagesIndex
                 }
-            }).then((response) => this.tvTitles = response.data.results);
+            }).then((response) => {
+                this.tvTitles = response.data.results;
+                this.totalPages = response.data.total_pages;
+            });
         },
         // Method to display the "Discover" section
         discoverTitles() {
             // Movies
             axios.get(this.baseUrl + "discover/movie", {
                 params: {
-                    api_key: this.apiKey
+                    api_key: this.apiKey,
+                    page: this.pagesIndex
                 }
-            }).then((response) => this.tvTitles = response.data.results);
+            }).then((response) => {
+                this.tvTitles = response.data.results;
+                this.totalPages = response.data.total_pages;
+            });
 
             // TV series
             axios.get(this.baseUrl + "discover/tv", {
                 params: {
-                    api_key: this.apiKey
+                    api_key: this.apiKey,
+                    page: this.pagesIndex
                 }
-            }).then((response) => this.tvTitles = [...this.tvTitles, ...response.data.results]);
+            }).then((response) => {
+                this.tvTitles = [...this.tvTitles, ...response.data.results];
+                this.totalPages = response.data.total_pages;
+            });
         },
         // Method to access navbar menu sections
         selectSection(section) {
+            // Resetting to first page when selecting other sections
+            this.otherSection = this.sectionSelected;
             this.sectionSelected = section;
-
+            (this.otherSection !== this.sectionSelected) ? this.pagesIndex = 1 : "";
+            
             // Selecting sections
             switch(this.sectionSelected) {
                 // Home
@@ -134,7 +164,31 @@ const app = new Vue({
         // Method to round up the original x/10 to a x/5 rating
         roundRating(num) {
             return num = Math.ceil(num / 2);
-        }   
+        },
+        // Method to access the previous page
+        prevPage(section) {
+            if (this.pagesIndex === 1) {
+                return
+            }
+            
+            // Decreasing page index
+            this.pagesIndex--;
+            
+            // Staying on the section selected
+            this.selectSection(section);
+        },
+        // Method to access the following page
+        nextPage(section) {
+            if (this.pagesIndex === this.totalPages) {
+                return
+            }
+
+            // Increasing page index
+            this.pagesIndex++;
+
+            // Staying on the section selected
+            this.selectSection(section);
+        }
     },
     mounted(){
         // Displaying weekly trending titles by default
